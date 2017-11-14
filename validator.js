f198d1b7db0d3156b57d80f3aa9015514c7ca24f
Chikito Emboaba <william@quicktech.no>
2017-11-14 13:48:26 -0200

    validation completion

--- validator.js
+++ validator.js
@@ -1,54 +1,55 @@
 function handle() {
-    var keycode = window.event.keyCode;
-    if (keycode == 13) {
+    const keycode = window.event.keyCode;
+    if (keycode === 13) {
         validate();
     }
 }
 
-
-let rules = {
+const rules = {
     name: 'required',
-    email: 'required|email', 
+    email: 'required|email',
     message: 'required'
 };
 
 function validate() {
 
-    document.querySelector('#input2 ~ .messagejs').classList.remove('add-error');
-    document.querySelector('#input3 ~ .messagejs').classList.remove('add-error');
-    document.querySelector('#input ~ .messagejs').classList.remove('add-error');
+    document.querySelector('#form-name ~ .messagejs').classList.remove('add-error');
+    document.querySelector('#form-email ~ .messagejs').classList.remove('add-error');
+    document.querySelector('#form-message ~ .messagejs').classList.remove('add-error');
     document.querySelector('button ~ .messagejs--success').classList.remove('add-error');
 
-    let name = document.getElementById("input2").value;
-    let email = document.getElementById("input3").value;
-    let message = document.getElementById("input").value;
+    const name = document.querySelector("#form-name").value
+        , email = document.querySelector("#form-email").value
+        , message = document.querySelector("#form-message").value
+        , data = { name, email, message };
+    
+    const validation = new Validator(data, rules);
 
-    let data = {
-        name: name,
-        email: email, 
-        message: message
-    };
-    
-    let validation = new Validator(data, rules);
-    
-    if(validation.passes() == true) {
+    if (validation.passes()) {
         document.querySelector('button ~ .messagejs--success').classList.add('add-error');
-        // document.querySelector('form').submit();
-    }
-    
-    if(validation.errors.first('name')) {
-        document.querySelector('#input2 ~ .messagejs').classList.add('add-error');
-        document.querySelector('#input2 ~ .messagejs').innerHTML = validation.errors.first('name');
-    } 
-    
-    if (validation.errors.first('email')) {
-        document.querySelector('#input3 ~ .messagejs').classList.add('add-error');
-        document.querySelector('#input3 ~ .messagejs').innerHTML = validation.errors.first('email');
-    } 
-    
-    if (validation.errors.first('message')) {
-        document.querySelector('#input ~ .messagejs').classList.add('add-error');
-        document.querySelector('#input ~ .messagejs').innerHTML = validation.errors.first('message');
+        return true;
     }
 
+    const errors = validation.errors.all()
+        , errorsKeys = Object.keys(errors);
+
+    errorsKeys.forEach(function (key) {
+        document.querySelector(`#form-${key} ~ .messagejs`).classList.add('add-error');
+        document.querySelector(`#form-${key} ~ .messagejs`).innerHTML = errors[key][0];
+    });
+
+    // if (validation.errors.first('name')) {
+    //     document.querySelector('#input2 ~ .messagejs').classList.add('add-error');
+    //     document.querySelector('#input2 ~ .messagejs').innerHTML = errors.name[0];
+    // } 
+    
+    // if (validation.errors.first('email')) {
+    //     document.querySelector('#input3 ~ .messagejs').classList.add('add-error');
+    //     document.querySelector('#input3 ~ .messagejs').innerHTML = errors.email[0];
+    // } 
+    
+    // if (validation.errors.first('message')) {
+    //     document.querySelector('#input ~ .messagejs').classList.add('add-error');
+    //     document.querySelector('#input ~ .messagejs').innerHTML = errors.message[0];
+    // }
 }
